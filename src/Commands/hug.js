5852487e260ddc6ada9c8e83338fb9dfbadf579f
PathonScript <pathonscript@gmail.com>
2021-09-14 10:48:45 +0700

    Added hugs

--- src/Commands/hug.js
+++ src/Commands/hug.js
@@ -3,10 +3,10 @@
 const Command = require("../Structures/Command.js");
 
 module.exports = new Command({
-	name: "hello",
-	description: "Hello!",
+	name: "hug",
+	description: "hugs!",
 	permission: "SEND_MESSAGES",
 	async run(message, args, client) {
-		message.reply("Hello Prick!");
+		message.reply("*hugs you*");
 	}
 });
